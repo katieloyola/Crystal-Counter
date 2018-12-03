08eab83dcb5b630ed642b4bcfb3a81af846851de
Katie Loyola <katie.loyola@gmail.com>
2018-12-03 16:39:51 -0700

    completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,95 @@
 //global variables
-var random_result;
-var lost;
-var win;
-var previous = 0;
+var randomNumber; //undefined
 
+//with numerical value
+var lose = 0;
+var win = 0;
+var previousNumber = 0;
 
-//random number between 19-120 - vocab: hoisting
-random_result = Math.floor(Math.random() * 102) + 19;
+//function to start game
+var startReset = function () {
 
-//to display random number on the page
-$("#result").html('The number you are aiming for is: ' + random_result);
+    //clear crystals values
+    $(".gems").empty();
 
-// display 4 crystals on the page (for loop)
-for (var i = 0; i < 4; i++) {
+    var image = ['https://vignette.wikia.nocookie.net/marvel-contestofchampions/images/1/10/Arena_Crystal.png/revision/latest?cb=20150825214845'
+    ];
 
-    //each crystal is assigned a random number between 1-12
-    var random = Math.floor(Math.random() * 12) + 1;
+    //generate random number between 19-120
+    randomNumber = Math.floor(Math.random() * 102) + 19;
 
-    var crystal = $("<div>");
-    crystal.attr({
-        "class": 'crystal',
-        "data-random": random
-    });
+    //to display random number on the page
+    $("#result").html('The number you are aiming for is: ' + randomNumber);
 
-    $(".crystals").append(crystal);
+    // display 4 crystals on the page (for loop)
+    for (var i = 0; i < 4; i++) {
+
+        //each crystal is assigned a random number between 1-12
+        var random = Math.floor(Math.random() * 12) + 1;
+        
+        //create div for random number within each crystal
+        var crystal = $("<div>");
+        crystal.attr({
+            "class": 'crystal',
+            "data-random": random,
+        });
+
+        crystal.css({
+            "background-image": "url('" + image + "')",
+            "background-size": "cover"
+        });
+
+        $(".gems").append(crystal);
+    }
+    //allows player to see addition on the page
+    $("#previous").html("Counter: " + previousNumber);
 }
 
-//when crystal is clicked, it will add a specific amount of points to the players total score
-$(".crystal").on('click', function () {
-    //vocab: THIS refers to the value of what was previously clicked ie. crystal
-    var result;
 
+//start game
+startReset();
+
+//when a crystal is clicked, it will add a specific amount of points to the players total score and reset to zero once number is reached 
+//vocab: event delegation allows us to attach a single event listener, to a parent element that will fire for all descendants matching a selector, whether those descendants exist now or will be added in the future.
+$(document).on('click', ".crystal", function () {
+
+    //vocab: THIS refers to the value of what was previously clicked ie. crystal
     //vocab: parseInt function parses a string and returns an integer
     var num = parseInt($(this).attr('data-random'));
-    // console.log(num, '+', result);
 
     //once clicked, update players score counter
-    previous += num;
-    console.log(previous);
+    previousNumber += num;
+    console.log(previousNumber);
+
+    $("#previous").html("Counter: " + previousNumber);
+
+
+    //insert conditional statement
+    //LOSES if their score goes above the random number
+    if (previousNumber > randomNumber) {
+        //add to score
+        lose++;
+
+        $("#lose").html("Total Losses: " + lose);
+
+        //when lose again, set previous variable to zero
+        previousNumber = 0;
+
+        //lose and start again
+        startReset();
+    }
 
     //WINS if total score equals random number from the beginning of the game
-    //insert conditional statement
-    if (previous > random_result) {
-        console.log("try again");
-    }
-    else if (previous === random_result) {
-        console.log("winner!");
-    }
+    else if (previousNumber === randomNumber) {
+        //add to score
+        win++;
 
+        $("#win").html("Total Wins: " + win);
+
+        //when win again, set previous variable to zero
+        previousNumber = 0;
+
+        //win and start again 
+        startReset();
+    }
 });
-
-
-
-//LOSES if their score goes above the random number
-//game restarts once player wins or loses
-//once game restarts, new random number appears
-//crystals will have all new hidden values
-//game should show number of games - wins and loses
-
-
-//each crystal should have a random hidden value between 1-12
